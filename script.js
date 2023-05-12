18e540889f4c0d082357d2c76460b51c0bad60b0
lolekmakso <makstarasovv1@gmail.com>
2023-05-12 17:49:21 +0300

    addad footer

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
-"use strict"
+"use strict";
 
-document.addEventListener('click', documentClick);
+document.addEventListener("click", documentClick);
 
 function documentClick(e) {
-	const targetItem  = e.target;
-	if (targetItem.closest('.icon-menu')){
-		document.documentElement.classList.toggle('menu-open');
-	}
+  const targetItem = e.target;
+  if (targetItem.closest(".icon-menu")) {
+    document.documentElement.classList.toggle("menu-open");
+  }
+  document.getElementById("body").classList.toggle("overflow-hidden");
+  document.getElementById("html").classList.toggle("overflow-hidden");
 }
+
+function checkSpinner() {
+  document.getElementById("spinner").classList.add("_active");
+
+  setTimeout(() => {
+    document.getElementById("spinner").classList.remove("_active");
+    document
+      .getElementById("desc")
+      .innerHTML = 'Spinner is finished';
+  }, 3000);
+}
+
+$(".owl-carousel").owlCarousel({
+  items: 3,
+  lazyLoad: true,
+  loop: false,
+  margin: 24,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+      dots: false,
+      margin: 0,
+    },
+    592: {
+      items: 2,
+      nav: false,
+    },
+    991: {
+      items: 3,
+      nav: false,
+    },
+  },
+});
